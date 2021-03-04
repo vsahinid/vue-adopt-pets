723f57ce9967e45bc3425caee493a745a8e5bae3
vsahinid <vsahinid@alumni.cmu.edu>
2021-03-03 18:18:00 -0600

    Added dogs and cats data

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,39 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Cats from "../views/Cats.vue";
+import Dogs from "../views/Dogs.vue";
+import Pet from "../views/Pet.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: "/dogs",
+    name: "Dogs",
+    component: Dogs
+  },
+  {
+    path: "/cats",
+    name: "Cats",
+    component: Cats
+  },
+  {
+    path: "/pet",
+    name: "Pet",
+    component: Pet
   }
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
