416885d477cfb888dc8c6b4fcb65884f1dc28bec
vsahinid <vsahinid@alumni.cmu.edu>
2021-03-03 19:38:52 -0600

    Updated app for stylig and index for routing

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/cats">Cats</router-link> |
-      <router-link to="/dogs">Dogs</router-link>
+      <router-link to="/dogs">Dogs</router-link> |
     </div>
     <b-container>
       <router-view/>
@@ -13,20 +13,17 @@
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-
 #nav {
   padding: 30px;
-
   a {
     font-weight: bold;
     color: #2c3e50;
-
     &.router-link-exact-active {
       color: #42b983;
     }
--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,35 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
+import Router from "vue-router";
 import Home from "../views/Home.vue";
 import Cats from "../views/Cats.vue";
 import Dogs from "../views/Dogs.vue";
 import Pet from "../views/Pet.vue";
 
-Vue.use(VueRouter);
+Vue.use(Router);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/dogs",
-    name: "Dogs",
-    component: Dogs
-  },
-  {
-    path: "/cats",
-    name: "Cats",
-    component: Cats
-  },
-  {
-    path: "/pets/:id",
-    name: "Pet",
-    component: Pet
-  }
-];
-
-const router = new VueRouter({
+export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/cats",
+      name: "cats",
+      component: Cats
+    },
+    {
+      path: "/dogs",
+      name: "dogs",
+      component: Dogs
+    },
+    {
+      path: "/pets/:species/:id",
+      name: "pet",
+      component: Pet
+    }
+  ]
 });
-
-export default router;
