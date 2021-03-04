5bf069d853702f09e124d4c7156714db8b5bfb91
vsahinid <vsahinid@alumni.cmu.edu>
2021-03-03 19:47:29 -0600

    Added styling to main and home

--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
 import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import BootstrapVue from "bootstrap-vue";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import "./assets/sass/index.sass";
+
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCat, faDog } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faCat, faDog);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.use(BootstrapVue);
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false;
 
